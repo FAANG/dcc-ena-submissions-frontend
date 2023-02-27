255eaab937322d09fa8121b8f2d8aec258563fd1
Yogmatee Roochun <yroochun@ebi.ac.uk>
2023-02-27 17:15:12 +0000

    point to prod backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  host: 'http://45.88.80.63:8000/',
+  host: 'https://api.faang.org/',
   relatedProjectsHost: 'https://raw.githubusercontent.com/FAANG/comm-data-portal-projects/master/projects/'
 };
