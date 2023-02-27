509a222f3b0bdf664f54836ad4f6fb91b888e1cf
Yogmatee Roochun <yroochun@ebi.ac.uk>
2023-02-27 16:20:44 +0000

    point to test backend

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  cookieDomain: 'data.faang.org',
-  envName: 'prod',
-  host: 'http://45.88.80.63:8000/',
-  relatedProjectsHost: 'https://raw.githubusercontent.com/FAANG/comm-data-portal-projects/master/projects/'
-};
